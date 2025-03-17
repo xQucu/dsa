a83b7dcd2a7e48d34be42519edb4eb8746b83e5f
Adrian Kucyk <kucykadrian2004@gmail.com>
2025-03-17 10:16:20 +0100

    feat: add palindrome checker using stack and linked list

---
+++ lab1/palindromes.py
@@ -0,0 +1,29 @@
+from arrayStack import Stack
+from singlyLinkedList import SinglyLinkedList
+
+
+def main() -> None:
+    s = input("Provide string to check if it is palindrome\n")
+    if isPalindrome(s):
+        print("It is a palindrome")
+    else:
+        print("It is not a palindrome")
+
+
+def isPalindrome(s: str) -> bool:
+    stack: Stack = Stack(len(s))
+    list: SinglyLinkedList = SinglyLinkedList()
+
+    for v in s:
+        stack.push(v)
+        list.add(v)
+
+    while list.size() > 0 and list.size() > 0:
+        if stack.pop() != list.remove():
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
