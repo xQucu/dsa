74c34fd0d4f6f0088ec732b06d43adf848a1c8b0
Adrian Kucyk <kucykadrian2004@gmail.com>
2025-03-17 19:22:41 +0100

    feat: add working toPostfixWithCorrection function

--- lab1/stack_and_RPN.py
+++ lab1/stack_and_RPN.py
@@ -1,16 +1,17 @@
 from arrayStack import Stack
 
 priority = {
-    "+": 2,
-    "-": 2,
-    "*": 1,
-    "/": 1,
+    "+": 4,
+    "-": 3,
+    "*": 2,
+    "/": 2,
+    "(": 1,
 }
 
 
 def main() -> None:
     infix_expression = input(
-        "Provide infix expression with reversed priority\n")
+        "Provide infix expression\n")
     processed_expression = toPostfixWithCorrection(infix_expression)
     print("Processed expression: ", processed_expression)
     evaluated_expression = evaluatePostfix(processed_expression)
@@ -19,9 +20,32 @@ def main() -> None:
 
 def toPostfixWithCorrection(e: str) -> str:
     tokens = e.split(" ")
+    operators: Stack[str] = Stack(len(e))
+    result = ""
     for t in tokens:
-        print(t, priority[t])
-    return ""
+        if t == "(":
+            operators.push(t)
+        elif t == ")":
+            while operators.top() != "(":
+                result += operators.pop()
+            operators.pop()
+        elif priority.get(t) == None:
+            result += t
+        else:
+            if operators.size() == 0:
+                operators.push(t)
+            elif priority[operators.top()] < priority[t]:
+                operators.push(t)
+            else:
+                result += operators.pop()
+                operators.push(t)
+
+    while operators.size() > 0:
+        # if operators.top() != "(":
+        result += operators.pop()
+        # operators.pop()
+
+    return result
 
 
 def evaluatePostfix(e: str) -> int:
